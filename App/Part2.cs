a48d5143fe2d36c9556a1119240841e44c312b7b
Aistis <aistis8624@gmail.com>
2014-04-14 22:50:59 +0300

    Removed unused code

--- App/Part2.cs
+++ App/Part2.cs
@@ -36,26 +36,7 @@ namespace App
 
             Console.WriteLine("\n\n");
 
-            Matrix loadedMatrixA = new Matrix(matrixD, "\n").addWith(new Matrix(matrixC, "\n").multiplyBy(k));
-            Matrix loadedMatrixB = new Matrix(matrixB, "\n");
-            Matrix loadedMatrixX = new Matrix(matrixX, "\n");
-
-            // some test data
-            //loadedMatrixA.matrix = new float[3, 3] {{2, 1, 0.95f}, {1, 2, 1}, {0.95f, 1, 2}};
-            //loadedMatrixB.matrix = new float[3, 1] {{3.95f}, {4}, {3.95f}};
-            //loadedMatrixX.matrix = new float[3, 1] { { 0 }, { 0 }, { 0 } };
-
-            JacobiMethod jacobiMethod = new JacobiMethod(
-                loadedMatrixA, loadedMatrixB, loadedMatrixX, epsilon
-            );
-
-            jacobiMethod.solve();
-
-            SteepestDescentMethod steepestDescentMethod = new SteepestDescentMethod(
-                loadedMatrixA, loadedMatrixB, loadedMatrixX, epsilon
-            );
-
-            steepestDescentMethod.solve();
+            Matrix loadedMatrixA = new Matrix(matrixT, "\n").addWith(new Matrix(matrixC, "\n").multiplyBy(k));
 
             Console.ReadKey();
         }
@@ -64,17 +45,11 @@ namespace App
         {
             Console.WriteLine();
 
-            Console.WriteLine("Matrix B");
-            Console.WriteLine(matrixB + "\n");
+            Console.WriteLine("Matrix T");
+            Console.WriteLine(matrixT + "\n");
 
             Console.WriteLine("Matrix C");
             Console.WriteLine(matrixC + "\n");
-
-            Console.WriteLine("Matrix D");
-            Console.WriteLine(matrixD + "\n");
-
-            Console.WriteLine("Matrix X");
-            Console.WriteLine(matrixX + "\n");
         }
     }
 }
